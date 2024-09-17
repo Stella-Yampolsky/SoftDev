c1eae70993dd37d253db6f6f792933e3310d252d
Stella <syampolsky50@stuy.edu>
2024-09-17 11:15:46 -0400

    finished

--- dictionary.py
+++ dictionary.py
@@ -1,6 +1,6 @@
 '''
 Stella Yampolsky
-Loonch
+JST
 SoftDev
 K04 -- Write a Python program to randomly select a "Devo" from a list in a dictionary.
 2024-09-13
@@ -8,20 +8,32 @@ time spent:
 '''
 import random
 krewes = {
-           4: [
-                'DUA','TAWAB','EVA','JACK','VICTOR','EVAN','JASON','COLYI','IVA'
-                'TAHMIM','STANLEY','LEON','NAOMI','NIA','ANASTASIA','JADY','BRI'
-                'ALEX','CHONGTIAN','DANNY','MARCO','ABIDUR','ANKITA','ANDY','ET'
-                'AIDAN','LINDA','QIANJUN','JIAYING','KISHI'
-                ],
-           5: [
-                'ADITYA','MARGIE','RACHEL','ALEXANDER','ZIYAD','DANNY','ENDRIT'
-                'VEDANT','SUHANA','KYLE','KEVIN','RAYMOND','CHRISTOPHER','JONAT'
-                'NAFIYU','TIM','WILL','DANIEL','BENJAMIN','CLAIRE','CHLOE','STE'
-                'JESSICA','JACKIE','WEN YUAN','YINWEI','TIFFANY','JAYDEN DANIEL>
+           4: [ 
+		'DUA','TAWAB','EVA','JACK','VICTOR','EVAN','JASON','COLYI','IVAN','TANZEEM',
+		'TAHMIM','STANLEY','LEON','NAOMI','NIA','ANASTASIA','JADY','BRIAN','JACOB',
+		'ALEX','CHONGTIAN','DANNY','MARCO','ABIDUR','ANKITA','ANDY','ETHAN','AMANDA',
+		'AIDAN','LINDA','QIANJUN','JIAYING','KISHI'
+		],
+           5: [ 
+                'ADITYA','MARGIE','RACHEL','ALEXANDER','ZIYAD','DANNY','ENDRIT','CADEN',
+                'VEDANT','SUHANA','KYLE','KEVIN','RAYMOND','CHRISTOPHER','JONATHAN','SASHA',
+                'NAFIYU','TIM','WILL','DANIEL','BENJAMIN','CLAIRE','CHLOE','STELLA','TRACY',
+                'JESSICA','JACKIE','WEN YUAN','YINWEI','TIFFANY','JAYDEN DANIEL','PRINCEDEN' 
               ]
          }
-pds = krewes.keys()
-pd = pds[random.randint(0, pds.length() - 1)]
-devo = krewes[pd[random.randint(0, pd.length() - 1)]]
-return devo         
+#takes a random period and then a random name using random choice
+def wayA():
+    pd = random.choice(list(krewes))
+    devo = random.choice(krewes[pd])
+    return devo
+
+
+#OR
+#takes a random period and then a random name using randint and indexing
+def wayB():
+    pds = list(krewes)
+    pd = pds[random.randint(0, len(pds) - 1)]
+    return krewes[pd][random.randint(0, len(pds) - 1)]
+
+print(wayA())
+print(wayB())
