028a927415fc9a303fda4a4e6d61f7c3f6b01fb5
Stella <syampolsky50@stuy.edu>
2024-09-26 11:47:17 -0400

    started

--- 11_flask_static/app.py
+++ 11_flask_static/app.py
@@ -8,29 +8,24 @@ import random
 from flask import Flask
 app = Flask(__name__)
 
-'''
+
 @app.route("/")
 def hello_world():
     print("the __name__ of this module is... ")
     print(__name__)
     return "No hablo queso!"
-'''
 
-'''
+
 @app.route("/static/foo.html")
 def h():
     print("the __name__ of this module is... ")
     print(__name__)
     return str(random.random())
-'''
 
-'''
-@app.route("/static/fixie.html")
-def h():
-    print("the __name__ of this module is... ")
-    print(__name__)
-    return str(random.random())
-'''
+
+
+
+
 
 
 if __name__ == "__main__":  # true if this file NOT imported
---
+++ 12_flask_jinja/app.py
@@ -0,0 +1,47 @@
+# Clyde 'Thluffy' Sinclair
+# SoftDev
+# Sep 2024
+
+"""
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Q0:
+<your response here>
+
+Q1:
+<your response here>
+
+Q2:
+<your response here>
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+"""
+
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Q0: What will happen if you remove render_template from the following statement?
+# (log prediction before executing...)
+# It won't load the template file (it's not in the path)
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "No hablo queso!"
+
+coll = [0,1,1,2,3,5,8]
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Q1: Can all of your teammates confidently predict the URL to use to load this page?
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+@app.route("/my_foist_template") 
+def test_tmplt():
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Q2: What is the significance of each argument? Simplest, most concise answer best.
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    return render_template( 'model_tmplt.html', foo="fooooo", collection=coll)
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
