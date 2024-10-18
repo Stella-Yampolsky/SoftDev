7ee8dc20bc23c34fb0d4b830ac78e3c79e5e25b2
Stella <syampolsky50@stuy.edu>
2024-10-18 11:47:29 -0400

    end of class

---
+++ 19_csv2db/build_db.py
@@ -0,0 +1,35 @@
+#Clyde "Thluffy" Sinclair
+#SoftDev
+#skeleton/stub :: SQLITE3 BASICS
+#Oct 2024
+
+import sqlite3   #enable control of an sqlite database
+import csv       #facilitate CSV I/O
+
+
+DB_FILE="discobandit.db"
+
+db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+c = db.cursor()               #facilitate db ops -- you will use cursor to trigger db events
+
+#==========================================================
+c.execute("create table students (one text, two int, three int)")
+c.execute("create table courses (one text, two int, three int)")
+with open("students.csv", newline='') as file:
+    r = csv.DictReader(file)    
+    for row in r:
+        c.execute(f"insert into students values('{row['name']}', '{row['age']}', '{row['id']}')")
+file.close
+with open("courses.csv", newline='') as file:
+    r = csv.DictReader(file)    
+    for row in r:
+        c.execute(f"insert into students values('{row['code']}', '{row['mark']}', '{row['id']}')")
+
+
+
+
+
+#==========================================================
+
+db.commit() #save changes
+db.close()  #close database
